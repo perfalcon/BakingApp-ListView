2ded60ee48f1cc739c2340663d6f4620284e2816
perfalcon <balaveer@gmail.com>
2018-08-08 06:48:59 -0400

    Espresso Test case - started-RecylerView- error

--- app/src/androidTest/java/com/example/balav/bakingapp_utils/RecipeSelectedTest.java
+++ app/src/androidTest/java/com/example/balav/bakingapp_utils/RecipeSelectedTest.java
@@ -1,5 +1,6 @@
 package com.example.balav.bakingapp_utils;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.runner.AndroidJUnit4;
 
 import android.support.test.rule.ActivityTestRule;
@@ -11,6 +12,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
@@ -18,17 +20,16 @@ import static org.hamcrest.core.IsNot.not;
 @RunWith(AndroidJUnit4.class)
 public class RecipeSelectedTest {
 
+
     @Rule
-    public ActivityTestRule<StepDetail> mActivityTestRule =
-            new ActivityTestRule<>(StepDetail.class);
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
 
     @Test
     public void OnRecipesDisplayedTest(){
-        //onView((withId(R.id.text_recipe))).check (matches(not(withText(""))));
 
-        onView((withId(R.id.btn_next_step))).perform(click());
-
-        onView((withId(R.id.tv_step_description))).check (matches(not(withText(""))));
+        onView(withId(R.id.rv_recipe)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.text_recipe)).check(matches(not(withText(""))));
 
     }
 
